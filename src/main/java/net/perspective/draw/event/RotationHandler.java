2f1bf19abad7e43c289cc0e5f77437e637a85bf2
ctipper <chris.tipper@gmail.com>
2020-02-13 16:50:00 +0000

    lines can now spin around axis in rotation mode

--- src/main/java/net/perspective/draw/event/RotationHandler.java
+++ src/main/java/net/perspective/draw/event/RotationHandler.java
@@ -12,6 +12,7 @@ import java.util.List;
 import javax.inject.Inject;
 import net.perspective.draw.CanvasView;
 import net.perspective.draw.DrawingArea;
+import net.perspective.draw.enums.DrawingType;
 import net.perspective.draw.geom.*;
 import net.perspective.draw.util.CanvasPoint;
 import net.perspective.draw.util.V2;
@@ -20,6 +21,7 @@ import net.perspective.draw.util.V2;
  * 
  * @author ctipper
  */
+
 public class RotationHandler implements Handler {
 
     @Inject
@@ -28,6 +30,7 @@ public class RotationHandler implements Handler {
     private CanvasView view;
     @Inject
     private DrawAreaListener listener;
+    private CanvasPoint st, en;
 
     public void upEvent() {
         if (view.getSelected() != -1) {
@@ -61,6 +64,13 @@ public class RotationHandler implements Handler {
                         break;
                     }
                 }
+                if ((item instanceof Figure) && ((Figure) item).getType().equals(FigureType.LINE)) {
+                    if (item.contains(listener.getStartX(), listener.getStartY())) {
+                        st = ((Figure) item).getStart();
+                        en = ((Figure) item).getEnd();
+                        view.setSelected(i);
+                    }
+                }
                 i--;
             } while (i >= 0);
         }
@@ -76,16 +86,10 @@ public class RotationHandler implements Handler {
             CanvasPoint centre = item.rotationCentre();
             A = B = new CanvasPoint(1, 1);
 
-            if (item instanceof Grouped) {
+            if ((item instanceof Figure)
+                    || (item instanceof Grouped)) {
                 A = new CanvasPoint(listener.getStartX() - centre.x, listener.getStartY() - centre.y);
                 B = new CanvasPoint(listener.getTempX() - centre.x, listener.getTempY() - centre.y);
-            }
-
-            if (item instanceof Figure) {
-                if (!((Figure) item).getType().equals(FigureType.LINE)) {
-                    A = new CanvasPoint(listener.getStartX() - centre.x, listener.getStartY() - centre.y);
-                    B = new CanvasPoint(listener.getTempX() - centre.x, listener.getTempY() - centre.y);
-                }
             }
 
             double h1 = V2.L2(A);
@@ -97,14 +101,29 @@ public class RotationHandler implements Handler {
             double sin_t = V2.dot(V2.rot90(q1), q2);
 
             double theta = Math.atan2(sin_t, cos_t);
-            item.setAngle(angle + theta);
 
-            angle = item.getAngle();
-            if (angle > Math.PI) {
-                item.setAngle(angle - 2 * Math.PI);
+            if (((item instanceof Figure) && (!((Figure) item).getType().equals(FigureType.LINE)))
+                    || (item instanceof Grouped)) {
+                item.setAngle(angle + theta);
+                // normalise angle
+                angle = item.getAngle();
+                if (angle > Math.PI) {
+                    item.setAngle(angle - 2 * Math.PI);
+                }
+                if (angle < -Math.PI) {
+                    item.setAngle(angle + 2 * Math.PI);
+                }
             }
-            if (angle < -Math.PI) {
-                item.setAngle(angle + 2 * Math.PI);
+            /**
+             * manipulate lines directly
+             */
+            if ((item instanceof Figure) && (((Figure) item).getType().equals(FigureType.LINE))) {
+                CanvasPoint s = this.rotate((Figure) item, st, theta);
+                CanvasPoint e = this.rotate((Figure) item, en, theta);
+                item.setStart(s.x, s.y);
+                item.setEnd(e.x, e.y);
+                ((Figure) item).setPoints(DrawingType.LINE);
+                ((Figure) item).setPath();
             }
 
             item.updateProperties(drawarea);
@@ -125,6 +144,26 @@ public class RotationHandler implements Handler {
         Rectangle2D rect;
         rect = new Rectangle2D.Double(p.x - 10.0, p.y - 10.0, 20.0, 20.0);
         return new Area(rect);
+    }
+
+    /**
+     * Rotate a point around figure axis by an angle
+     * 
+     * @param figure
+     * @param p
+     * @param angle
+     * @return 
+     */
+    protected CanvasPoint rotate(Figure figure, CanvasPoint p, double angle) {
+        CanvasPoint centre = figure.rotationCentre();
+        CanvasPoint point = new CanvasPoint(p.x, p.y);
+        point.translate(-centre.x, -centre.y);
+        if (angle != 0) {
+            // rotate point about centroid
+            point.rotate(angle);
+        }
+        point.translate(centre.x, centre.y);
+        return point;
     }
 
 }
