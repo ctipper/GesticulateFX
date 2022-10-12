bf3163aba02e8dd7d7d960e2c43e685621bca788
ctipper <chris.tipper@gmail.com>
2022-10-12 21:41:45 +0100

    activate system theme detector library

--- src/main/java/net/perspective/draw/Gesticulate.java
+++ src/main/java/net/perspective/draw/Gesticulate.java
@@ -78,7 +78,7 @@ public class Gesticulate extends GuiceApplication {
     private final int frameTop = (int) (screenSize.getMaxY() - sceneHeight) / 5;
 
     private static final boolean MAC_OS_X = System.getProperty("os.name").toLowerCase().startsWith("mac os x");
-    protected static final boolean MM_SYSTEM_THEME = false;
+    protected static final boolean MM_SYSTEM_THEME = true;
 
     private static final Logger logger = LoggerFactory.getLogger(Gesticulate.class.getName());
 
