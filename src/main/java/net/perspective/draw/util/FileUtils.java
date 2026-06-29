39a98b6e30d51f2c815e09f969e82086b89415b4
ctipper <chris.tipper@gmail.com>
2026-06-29 10:34:10 +0100

    update image file extensions to include .jpeg

--- src/main/java/net/perspective/draw/util/FileUtils.java
+++ src/main/java/net/perspective/draw/util/FileUtils.java
@@ -40,14 +40,6 @@ import org.slf4j.LoggerFactory;
 
 public class FileUtils {
 
-    public final static String jpeg = "jpeg";
-    public final static String jpg = "jpg";
-    public final static String gif = "gif";
-    public final static String tiff = "tiff";
-    public final static String tif = "tif";
-    public final static String png = "png";
-    public final static String svg = "svg";
-
     private static final Logger logger = LoggerFactory.getLogger(FileUtils.class.getName());
 
     private FileUtils() {
