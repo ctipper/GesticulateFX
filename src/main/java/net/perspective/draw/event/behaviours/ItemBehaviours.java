5dc1bad1795bcd93b6c5f7d5b3a2f7316e8f2cfc
ctipper <chris.tipper@gmail.com>
2026-08-27 12:10:11 +0100

    document itembehaviours

--- src/main/java/net/perspective/draw/event/behaviours/ItemBehaviours.java
+++ src/main/java/net/perspective/draw/event/behaviours/ItemBehaviours.java
@@ -33,12 +33,41 @@ import net.perspective.draw.geom.DrawItem;
 
 public interface ItemBehaviours {
 
+    /**
+     * Select the item, setting the containment type on the context
+     *
+     * @param context  a {@link net.perspective.draw.event.behaviours.BehaviourContext}
+     * @param item  a {@link net.perspective.draw.geom.DrawItem}
+     * @param id  the slot id of the item
+     * @return item is selected
+     */
     boolean selectItem(BehaviourContext context, DrawItem item, ItemId id);
 
+    /**
+     * Enter the item's editing mode
+     *
+     * @param context  a {@link net.perspective.draw.event.behaviours.BehaviourContext}
+     * @param item  a {@link net.perspective.draw.geom.DrawItem}
+     * @param id  the slot id of the item
+     */
     void editItem(BehaviourContext context, DrawItem item, ItemId id);
 
+    /**
+     * Set the cursor for the pointer's position over the item
+     *
+     * @param context  a {@link net.perspective.draw.event.behaviours.BehaviourContext}
+     * @param item  a {@link net.perspective.draw.geom.DrawItem}
+     */
     void hoverItem(BehaviourContext context, DrawItem item);
 
+    /**
+     * Transform the item according to the containment type set by selection
+     *
+     * @param context  a {@link net.perspective.draw.event.behaviours.BehaviourContext}
+     * @param item  a {@link net.perspective.draw.geom.DrawItem}
+     * @param xinc  an x increment
+     * @param yinc  a y increment
+     */
     void alterItem(BehaviourContext context, DrawItem item, double xinc, double yinc);
 
 }
